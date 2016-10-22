960664f995b9418571b62f93c461c67ea35a2ef0
zscgrhg <zscgrhg@qq.com>
2016-10-22 19:30:15 +0800

    boot gradle react

--- src/main/java/com/example/BootmanApplication.java
+++ src/main/java/com/example/BootmanApplication.java
@@ -1,12 +1,17 @@
 package com.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class BootmanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootmanApplication.class, args);
+		while (true){
+			log.debug("this is test log rolling");
+		}
 	}
 }
